2549d563fab656a7df02e7c9913f67ae13818424
Iaroslav Rodichev <i.rodichev@vislab.team>
2025-10-11 18:17:25 +0300

    physics and timeline

--- engine/src/Engine.cpp
+++ engine/src/Engine.cpp
@@ -1,12 +1,14 @@
-#include "pch.h"
 #include "Engine.h"
+#include "pch.h"
+
 
 #include "VGJS.h"
 
 #include "aliases.h"
-#include "subsystems/RenderSubsystem.h"
 #include "subsystems/Log.h"
+#include "subsystems/RenderSubsystem.h"
 #include "tools/Profiler.h"
+
 
 #include "tools/Timeline.h"
 
@@ -14,12 +16,13 @@ using namespace Blainn;
 
 std::shared_ptr<vgjs::JobSystem> Engine::m_jobSystemPtr = nullptr;
 
-void Engine::Init() {
-	//TODO: Initialize engine subsystems here
+void Engine::Init()
+{
+    // TODO: Initialize engine subsystems here
 
     vgjs::thread_count_t jobSystemThreadCount{8};
     m_jobSystemPtr = std::make_shared<vgjs::JobSystem>(vgjs::JobSystem(jobSystemThreadCount));
-    
+
     //   just to check that cmake is working
     Blainn::RenderSubsystem::Init();
 
@@ -32,29 +35,25 @@ void Engine::Shutdown()
 
 void Engine::Run()
 {
-    float marioPeriod =  1000.0 / 2.0;
-    Timeline<eastl::chrono::milliseconds> sayMarioTimeline(marioPeriod);
+    Timeline<eastl::chrono::milliseconds> sayMarioTimeline;
     sayMarioTimeline.Start();
 
-    Timeline<eastl::chrono::milliseconds> mainTimeline(1000.0 / 60.0);
+    Timeline<eastl::chrono::milliseconds> mainTimeline;
     mainTimeline.Start();
-
-    Timeline<eastl::chrono::milliseconds> physicsTimeline(m_physicsUpdatePeriodMs);
-    physicsTimeline.Start();
 
     bool isRunning = true;
     while (isRunning)
     {
-        float mainTimelineDeltaTime =  mainTimeline.Tick();
+        float mainTimelineDeltaTime = mainTimeline.Tick();
         float marioTimelineDeltaTime = sayMarioTimeline.Tick();
         static float marioAccumulator;
         marioAccumulator += mainTimelineDeltaTime;
-        if (marioAccumulator >= marioPeriod)
+        if (marioAccumulator >= 500.0f)
         {
             std::cout << "Hello, it's me, Marrrriooooooo!" << std::endl;
             marioAccumulator = 0;
         }
-        
+
         // this trace doesn't make sense, it exactly matches the frame
         BLAINN_PROFILE_SCOPE_DYNAMIC("Main loop");
 
@@ -69,5 +68,4 @@ void Engine::Run()
         // Marks end of frame for tracy profiler
         BLAINN_PROFILE_MARK_FRAME;
     }
-
 }
