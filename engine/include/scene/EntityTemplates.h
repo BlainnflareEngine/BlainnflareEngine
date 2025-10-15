dc44411ee14292fbc2567b726ee04f22f7dcd623
WhoLeb <gleb_lyah@mail.ru>
2025-10-15 20:36:04 +0300

    Add entity class and its body. Add primitive components.

---
+++ engine/include/scene/EntityTemplates.h
@@ -0,0 +1,88 @@
+#pragma once
+
+namespace Blainn
+{
+
+template<typename T, typename... Args>
+T& Entity::AddComponent(Args&&... args)
+{
+    assert(HasComponent<T>() || "Entity doesn't have the component!");
+    return m_Scene->m_Registry.emplace<T>(m_EntityHandle, std::forward<Args>(args)...);
+}
+
+template<typename T>
+T& Entity::GetComponent() const
+{
+    assert(HasComponent<T>() || "Entity doesn't have the component!");
+    return m_Scene->m_Registry.get<T>(m_EntityHandle);
+}
+
+// Returns nullptr if entity does not have the requested component
+template<typename T>
+T* Entity::TryGetComponent()
+{
+    assert(IsValid());
+    return m_Scene->m_Registry.try_get<T>(m_EntityHandle);
+}
+
+// Returns nullptr if entity does not have the requested component
+template<typename T>
+const T* Entity::TryGetComponent() const
+{
+    assert(IsValid());
+    return m_Scene->m_Registry.try_get<T>(m_EntityHandle);
+}
+
+template<typename T>
+bool Entity::HasComponent()
+{
+    assert(IsValid());
+    return m_Scene->m_Registry.has<T>(m_EntityHandle);
+}
+template<typename T>
+bool Entity::HasComponent() const
+{
+    assert(IsValid());
+    return m_Scene->m_Registry.has<T>(m_EntityHandle);
+}
+
+template<typename... T>
+bool Entity::HasAny()
+{
+    assert(IsValid());
+    return m_Scene->m_Registry.any_of<T...>(m_EntityHandle);
+}
+template<typename... T>
+bool Entity::HasAny() const
+{
+    assert(IsValid());
+    return m_Scene->m_Registry.any_of<T...>(m_EntityHandle);
+}
+
+template<typename... T>
+bool Entity::HasAll()
+{
+    assert(IsValid());
+    return m_Scene->m_Registry.all_of<T...>(m_EntityHandle);
+}
+template<typename... T>
+bool Entity::HasAll() const
+{
+    assert(IsValid());
+    return m_Scene->m_Registry.all_of<T...>(m_EntityHandle);
+}
+
+template<typename T>
+void Entity::RemoveComponent()
+{
+    assert(HasComponent<T>() || "Entity doesn't have the component!");
+    return m_Scene->m_Registry.erase<T>(m_EntityHandle);
+}
+template<typename T>
+void Entity::RemoveComponentIfExists()
+{
+    assert(IsValid());
+    return m_Scene->m_Registry.remove<T>(m_EntityHandle);
+}
+
+}
---
+++ engine/src/scene/Entity.cpp
@@ -0,0 +1,90 @@
+//
+// WhoLeb
+//
+#include "pch.h"
+
+#include "scene/Entity.h"
+#include "scene/Scene.h"
+
+using namespace Blainn;
+
+bool Entity::IsValid() const
+{
+    return (m_EntityHandle != entt::null)
+        && m_Scene
+        && m_Scene->m_Registry.valid(m_EntityHandle);
+}
+
+
+Entity::operator bool() const
+{
+    return IsValid();
+}
+
+
+uuid Entity::GetSceneUUID() const
+{
+    return m_Scene->m_SceneID;
+}
+
+
+Entity Entity::GetParent() const
+{
+    return m_Scene->TryGetEntityWithUUID(GetParentUUID());
+}
+
+
+void Entity::SetParent(Entity parent)
+{
+    Entity currentParent = GetParent();
+    if (currentParent == parent)
+        return;
+
+    if (currentParent)
+        currentParent.RemoveChild(*this);
+
+    SetParentUUID(parent.GetUUID());
+
+    if (parent)
+    {
+        auto& parentChildren = parent.Children();
+        uuid _uuid = GetUUID();
+        if (eastl::find(parentChildren.begin(), parentChildren.end(), _uuid) == parentChildren.end())
+            parentChildren.emplace_back(GetUUID());
+    }
+}
+
+
+bool Entity::RemoveChild(Entity child)
+{
+    uuid childId = child.GetUUID();
+    auto& children = Children();
+    auto it = eastl::find(children.begin(), children.end(), childId);
+    if (it)
+    {
+        children.erase(it);
+        return true;
+    }
+    return false;
+}
+
+
+bool Entity::IsAncestorOf(Entity entity)
+{
+    const auto& children = Children();
+    uuid childId = entity.GetUUID();
+
+    if (children.empty())
+        return false;
+
+    for (uuid child : children)
+        if (child == childId)
+            return true;
+
+    for (uuid child : children)
+        if (m_Scene->GetEntityWithUUID(child).IsAncestorOf(entity))
+            return true;
+
+    return false;
+}
+
