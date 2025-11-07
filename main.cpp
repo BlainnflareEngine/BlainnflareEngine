6ccead426880490c0597962da78e37a67b0c582b
jesusdoesnotloveyou <artyomkozlovsky@gmail.com>
2025-11-07 13:27:31 +0300

    main loop significantly changed (standalone launch)

--- main.cpp
+++ main.cpp
@@ -8,6 +8,15 @@
 #include "engine/include/Engine.h"
 #include "engine/include/tools/Timeline.h"
 
+#define MAX_NAME_STRING 256
+#define HInstance() GetModuleHandle(NULL)
+
+INT WindowWidth;
+INT WindowHeight;
+
+CHAR WindowTitle[MAX_NAME_STRING];
+CHAR WindowClass[MAX_NAME_STRING];
+
 int main(int argc, char **argv)
 {
     BF_DEBUG("This is debug!");
@@ -16,31 +25,70 @@ int main(int argc, char **argv)
     BF_WARN("This is warn!");
     BF_TRACE("This is trace!");
 
+//#if !defined(BLAINN_INCLUDE_EDITOR)
+    strcpy_s(WindowTitle, "Blainnflare Engine");
+    strcpy_s(WindowClass, "D3D12SampleClass");
+
+    WindowWidth = 1280;
+    WindowHeight = 720;
+//#endif
+
     Blainn::Engine::Init();
+    HWND hwnd = NULL;
 
-#if defined(BLAINN_INCLUDE_EDITOR)
-    // needed for qt to generate resources (icons etc.)
-    Q_INIT_RESOURCE(sources);
-
-    Blainn::Editor::GetInstance().Init(argc, argv);
-    Blainn::Editor::GetInstance().Show();
-#endif
-
-    HWND hwnd = Blainn::Editor::GetInstance().GetViewportHWND();
+//#if defined(BLAINN_INCLUDE_EDITOR)
+//    // needed for qt to generate resources (icons etc.)
+//    Q_INIT_RESOURCE(sources);
+//
+//    Blainn::Editor::GetInstance().Init(argc, argv);
+//    Blainn::Editor::GetInstance().Show();
+//    
+//    hwnd = Blainn::Editor::GetInstance().GetViewportHWND();
+//#else
+    hwnd = Blainn::Engine::CreateBlainnWindow(WindowWidth, WindowHeight, WindowTitle, WindowClass, HInstance());
+    //#endif
+    
     Blainn::Engine::InitRenderSubsystem(hwnd);
 
+    bool isRunning = true;
+   
+    MSG msg = {0};
     Blainn::Timeline<eastl::chrono::milliseconds> mainTimeline;
     mainTimeline.Start();
-
-    bool isRunning = true;
+    
     while (isRunning)
     {
-        float mainTimelineDeltaTime = mainTimeline.Tick();
-        Blainn::Engine::Update(mainTimelineDeltaTime);
+        // Process any messages in the queue.
+        if (PeekMessage(&msg, NULL, 0u, 0u, PM_REMOVE))
+        {
+            TranslateMessage(&msg);
+            DispatchMessage(&msg);
+        }
+        else
+        {
+            // fps stats could be added
+            float mainTimelineDeltaTime = mainTimeline.Tick();
+            
+            //if (!m_appPaused)
+            {
+                Blainn::Engine::Update(mainTimelineDeltaTime);
+// #if defined(BLAINN_INCLUDE_EDITOR)
+                // Blainn::Editor::GetInstance().Update();
+// #endif
+            }
+            //else
+            {
 
-#if defined(BLAINN_INCLUDE_EDITOR)
-        Blainn::Editor::GetInstance().Update();
-#endif
+            }
+
+            if (msg.message == WM_QUIT)
+            {
+                isRunning = false;
+            }
+
+        }
+
+
     }
 
     Blainn::Editor::GetInstance().Destroy();
