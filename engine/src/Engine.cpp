cd56a9abc0ae4c6b7bca4abb2220f3d1b939155a
WhoLeb <gleb_lyah@mail.ru>
2026-01-15 12:13:51 +0300

    remove extra picking

--- engine/src/Engine.cpp
+++ engine/src/Engine.cpp
@@ -43,19 +43,6 @@ void Engine::Init(Timeline<eastl::chrono::milliseconds> &globalTimeline)
     AssetManager::GetInstance().Init();
     ScriptingSubsystem::Init();
     AISubsystem::GetInstance().Init();
-
-    Input::AddEventListener(InputEventType::MouseButtonPressed, [&](const InputEventPointer &event)
-    {
-        const MouseButtonPressedEvent *mouseEvent = static_cast<const MouseButtonPressedEvent*>(event.get());
-        auto button = mouseEvent->GetMouseButton();
-
-        if (button == MouseButton::Left)
-        {
-            uuid id = RenderSubsystem::GetInstance().GetUUIDAt(mouseEvent->GetX() - 350, mouseEvent->GetY() - 130);
-
-            BF_INFO("the picked id is {}", id.str());
-        }
-    });
     NavigationSubsystem::Init();
     NavigationSubsystem::SetShouldDrawDebug(true);
 }
