16702ebfb31e4e5e0fb2b6da6e2c56e978926be4
jesusdoesnotloveyou <artyomkozlovsky@gmail.com>
2026-01-22 01:09:45 +0300

    prebuilt meshes extended

--- engine/include/Render/PrebuiltEngineMeshes.h
+++ engine/include/Render/PrebuiltEngineMeshes.h
@@ -5,6 +5,15 @@
 
 namespace Blainn
 {
+    enum EPrebuiltMeshType : uint8_t
+    {
+        CUBE = 0u,
+        SPHERE,
+        GRID,
+        GEOSPHERE,
+        NUM_PREBUILT_MESHES
+    };
+
     class PrebuiltEngineMeshes
     {
     public:
@@ -15,5 +24,9 @@ namespace Blainn
         static MeshData<> CreateGrid(float width, float depth, UINT m, UINT n);
 
         static MeshData<> CreateGeosphere(float radius, UINT numSubdivisions);
+
+    private:
+        static void Subdivide(MeshData<> &meshData);
+        static BlainnVertex MidPoint(const BlainnVertex &v0, const BlainnVertex &v1);
     };
 }
