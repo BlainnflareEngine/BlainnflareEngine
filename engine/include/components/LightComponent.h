4df6b5c03230a28ea4ff078d709b065b08a46d45
jesusdoesnotloveyou <artyomkozlovsky@gmail.com>
2026-01-22 15:00:33 +0300

    light component initial

--- engine/include/components/LightComponent.h
+++ engine/include/components/LightComponent.h
@@ -2,18 +2,38 @@
 
 #include "aliases.h"
 #include "Handles/Handle.h"
+#include "Render/FreyaCoreTypes.h"
 
 namespace Blainn
 {
+    enum class ELightType
+    {
+        NONE = -1,
+        Directional,
+        Point,
+        Spot,
+        NumLightTypes
+    };
+
     struct LightComponent
     {
         LightComponent()
         {
+
         }
 
-        LightComponent()
+        LightComponent(const eastl::shared_ptr<MeshHandle> &meshHandle)
         {
+            MeshHandle = meshHandle;
         }
 
+        LightComponent(eastl::shared_ptr<MeshHandle>&& meshHandle)
+        {
+            MeshHandle = eastl::move(meshHandle);
+        }
+
+        eastl::shared_ptr<MeshHandle> MeshHandle;
+        LightData Light;
+        ELightType LightType = ELightType::NONE;
     };
 } // namespace Blainn
