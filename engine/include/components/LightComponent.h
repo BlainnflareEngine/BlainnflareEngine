b7728e07fcc481b2a75dfbecc62ec14fef672589
GoreToska <gorevoi_ivan@mail.ru>
2026-01-25 00:51:45 +0300

    Light component copy & move constructor defaulted

--- engine/include/components/LightComponent.h
+++ engine/include/components/LightComponent.h
@@ -6,85 +6,72 @@
 
 namespace Blainn
 {
-    enum class ELightType
+enum class ELightType
+{
+    Directional,
+    Point,
+    Spot,
+    NumLightTypes
+};
+
+struct LightComponent
+{
+private:
+    inline static const int kNumFramesMarkDirty = 3;
+
+    // dirty flag available between frames
+    int NumFramesDirty = kNumFramesMarkDirty; // NumFrameResources
+
+public:
+    LightComponent()
     {
-        Directional,
-        Point,
-        Spot,
-        NumLightTypes
+    }
+
+    void MarkFramesDirty()
+    {
+        NumFramesDirty = kNumFramesMarkDirty;
     };
 
-    struct LightComponent
+    bool IsFramesDirty() const
     {
-    private:
-        inline static const int kNumFramesMarkDirty = 3;
+        return NumFramesDirty > 0;
+    }
 
-        // dirty flag available between frames
-        int NumFramesDirty = kNumFramesMarkDirty; // NumFrameResources
-
-    public:
-        LightComponent()
-        {
-        }
-
-        LightComponent(const LightComponent& other)
-        {
-
-        }
-
-        LightComponent(LightComponent &&other) noexcept
-        {
-
-        }
-
-        // call from qt
-        void MarkFramesDirty()
-        {
-            NumFramesDirty = kNumFramesMarkDirty;
-        };
-
-        bool IsFramesDirty() const
-        {
-            return NumFramesDirty > 0;
-        }
-
-        void FrameResetDirtyFlags()
-        {
-            NumFramesDirty > 0 ? --NumFramesDirty : NumFramesDirty;
-        }
-
-        XMFLOAT4 Color = {1.0f, 1.0f, 1.0f, 1.0f};
-    };
-
-    struct DirectionalLightComponent : public LightComponent
+    void FrameResetDirtyFlags()
     {
-        DirectionalLightComponent()
-        {
+        NumFramesDirty > 0 ? --NumFramesDirty : NumFramesDirty;
+    }
 
-        }
-    };
+    XMFLOAT4 Color = {1.0f, 1.0f, 1.0f, 1.0f};
+};
 
-    struct PointLightComponent : public LightComponent
+struct DirectionalLightComponent : public LightComponent
+{
+    DirectionalLightComponent()
     {
-        PointLightComponent()
-        {
+    }
+};
 
-        }
-
-        float FalloffEnd = 1; // Range
-        float FalloffStart = 0.1;
-    };
-
-    struct SpotLightComponent : public LightComponent
+struct PointLightComponent : public LightComponent
+{
+    PointLightComponent()
     {
-        SpotLightComponent()
-        {
-        }
+    }
 
-        //XMFLOAT3 Direction = {0.5f, -1.0f, 0.5f};
-        float Range;
-        float SpotInnerAngle;
-        float SpotOuterAngle;
-    };
+    float FalloffEnd = 1; // Range
+    float FalloffStart = 0.1;
+};
+
+struct SpotLightComponent : public LightComponent
+{
+    SpotLightComponent()
+    {
+    }
+
+    // XMFLOAT3 Direction = {0.5f, -1.0f, 0.5f};
+    float Range;
+    float SpotInnerAngle;
+    float SpotOuterAngle;
+};
 
 } // namespace Blainn
